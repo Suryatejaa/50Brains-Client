7564841780eb0dc7f150b924476318c2c552c1f6
EchoLift <echoliftagency@gmail.com>
2025-10-25 17:59:18 +0530

    Implemented Analytics and speed insights

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { Providers } from '@/components/providers';
-import { SpeedInsights } from "@vercel/speed-insights/next"
-import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata: Metadata = {
   title: {
